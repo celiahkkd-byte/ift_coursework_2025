5d754d9bc927c932b17d5eb76f06094232ff0609
celia-BO HUANG <celiahkkd@gmail.com>
2026-02-15 01:06:03 +0000

    refactor: unify output loader to load.py only

--- team_Pearson/coursework_one/modules/output/__init__.py
+++ team_Pearson/coursework_one/modules/output/__init__.py
@@ -1,6 +1,5 @@
 from .load import load_curated
-from .loader import load_records
 from .normalize import normalize_records
 from .quality import run_quality_checks
 
-__all__ = ["normalize_records", "run_quality_checks", "load_curated", "load_records"]
+__all__ = ["normalize_records", "run_quality_checks", "load_curated"]
