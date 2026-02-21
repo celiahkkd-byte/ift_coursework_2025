384b29af33c5d806f4996dbd6549f1753b7abbdc
Gu <essieeeee@163.com>
2026-02-21 15:44:23 +0000

    Role5: DB connectivity, universe extraction, sphinx documentation

---
+++ team_Pearson/coursework_one/docs/sphinx/source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+
+REPO_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+CW1_ROOT = os.path.join(REPO_ROOT, "team_Pearson", "coursework_one")
+
+sys.path.insert(0, CW1_ROOT)
+sys.path.insert(0, REPO_ROOT)
+# --- FIX for autodoc (ensure coursework_one/modules is importable) ---
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..")))          # coursework_one
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "modules")))  # coursework_one/modules
+
+project = 'IFT Coursework 2025_team_Pearson'
+copyright = '2026, Team Pearson'
+author = 'Team Pearson'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "myst_parser",
+]
+autodoc_mock_imports = ["psycopg2"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
